1d8f03fd897c18b23db42750585f49d64109aa2b
Brian Kloppenborg <brian@kloppenborg.net>
2010-02-17 17:24:38 +0000

    More oCL stuff
    git-svn-id: http://192.168.1.75/svn/gpair@6 48422db7-73d2-4338-a790-e04df5fead5b

--- gpu.c
+++ gpu.c
@@ -9,42 +9,32 @@ void print_opencl_error(char* error_message, int error_code)
     exit(0);
 }
 
+// A function to setup the OpenCL device with all of our kernels
+void setup_device()
+{
+    cl_device_id device_id;           // device ID
+    cl_context context;               // context
+    cl_command_queue queue;           // command queue
+    cl_program program;               // program
+    cl_kernel kernel;                 // kernel
+}
+
+
 // Compute the chi2 of the data using a GPU
 double data2chi2_gpu()
 {
-    double chi2 = 0., chi2v2 = 0., chi2bs = 0. ;
-    double complex t3;
-    //  double t3r, t3i;
-    register int ii;
+    // TODO: Pass pointers into this function.
+    // TODO: Move this code out of this function and just keep the data on the GPU:
+    // Move the necessary data over to the GPU
     
-    // Variables used in the loop below:
-    // current.t3
-    // data.bisphs
-    // data.bisamp
-    // data.bisamperr
     
-    // Functions used below:
-    // cimag
-    // square
+    // Now compute the chi2 for the V2
+    double chi2v2 = chi2v2_gpu();
+    
+    // And now the chi2 for the bispectrum:
+    double chi2bs = chi2bs_gpu();
 
-    for(ii =0; ii < data.nbis ; ii++)
-    {
-      
-      t3 = ( current.t3[ ii ] * cexp( - I * data.bisphs[ii] )   - data.bisamp[ii] ) ;
-      t3 = creal(t3) / data.bisamperr[ii] + I * cimag(t3) / ( data.bisamp[ii] * data.bisphserr[ii] );
-      chi2bs += square( cabs(t3 ) );
-      // alternative
-      // data.t3ierr[ii] = data.bisamp[ii] * data.bisphserr[ii];
-      // data.t3rerr[ii] = data.bisamperr[ii];
-      //  data.t3phasor[ii] = cexp( - I * data.bisphs[ii] )
-      // t3= ( current.t3[ ii ] * data.t3phasor[ ii ]   - data.bisamp[ii] ) ;
-      // t3r = creal(t3) / data.realerr[ii];
-      // t3i = cimag(t3) / data.imagerr[ii];
-      // chi2bs += t3r * t3r + t3i * t3i;
-    } 
-
-    chi2 = chi2v2 + chi2bs;
-    return chi2/(double)( data.npow + 2.*data.nbis);
+    return (chi2v2 + chi2bs) / (double)( data.npow + 2.*data.nbis);
 }
 
 // Computes the chi2 for the visibilities on the GPU
@@ -61,4 +51,26 @@ double chi2v2_gpu()
       chi2v2 += square( ( current.pow[ii] - data.pow[ii] ) / data.powerr[ii] ) ;
     }
 
+}
+
+// Computes the chi2 for the bispectrum on the GPU
+double chi2bs_gpu()
+{
+    double complex;
+    double t3;
+    for(ii =0; ii < data.nbis ; ii++)
+    {
+      
+      t3 = ( current.t3[ ii ] * cexp( - I * data.bisphs[ii] )   - data.bisamp[ii] ) ;
+      t3 = creal(t3) / data.bisamperr[ii] + I * cimag(t3) / ( data.bisamp[ii] * data.bisphserr[ii] );
+      chi2bs += square( cabs(t3 ) );
+      // alternative
+      // data.t3ierr[ii] = data.bisamp[ii] * data.bisphserr[ii];
+      // data.t3rerr[ii] = data.bisamperr[ii];
+      //  data.t3phasor[ii] = cexp( - I * data.bisphs[ii] )
+      // t3= ( current.t3[ ii ] * data.t3phasor[ ii ]   - data.bisamp[ii] ) ;
+      // t3r = creal(t3) / data.realerr[ii];
+      // t3i = cimag(t3) / data.imagerr[ii];
+      // chi2bs += t3r * t3r + t3i * t3i;
+    } 
 }
